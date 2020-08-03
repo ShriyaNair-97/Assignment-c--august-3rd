d7068981289065f82f21d303b60973383c9bd32a
Shriya nair <shriya.nair@zensar.com>
2020-08-03 18:30:51 +0530

    Add files to the repository

---
+++ ConsoleAnimal/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAnimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] MyArray = { "Zebra", "Ant", "Owl", "Lion", "Tiger" };
+            Console.WriteLine("\nDisplaying the names in MyArray by For loop");
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                
+                Console.WriteLine("\n",MyArray[i]);
+            }
+            Console.WriteLine("\n-----------------------------------");
+            Console.WriteLine("\nDisplaying the names in MyArray by ForEach loop");
+            foreach (string a in MyArray)
+            {
+               
+                Console.WriteLine("\n",a);
+            }
+            Console.WriteLine("\n------------------------------------");
+            Console.WriteLine("\nDisplaying the names in MyArray by Do-While loop");
+            int j = 0;
+            do
+            {
+               
+                Console.WriteLine(MyArray[j]);
+                j++;
+            } while (j <= MyArray.Length);
+            Console.WriteLine("\n-------------------------------------");
+            int k = 0;
+            Console.WriteLine("\nDisplaying the names in MyArray by Do-While loop");
+            while (k < MyArray.Length)
+            {
+              
+                Console.WriteLine("\n",MyArray[k]);
+                k++;
+            }
+            Console.WriteLine("\n-------------------------------------");
+            Array.Sort(MyArray);
+            Console.WriteLine("\nSorted List");
+            for (int l = 0; l < MyArray.Length; l++)
+            {
+                Console.Write(MyArray[l] + " ");
+            }
+            Console.WriteLine("\n-------------------------------------");
+            string[] Animal = { "Zebra", "Kiwi", "Amazona Parrot", "Tiger" };
+
+            Array.Sort(Animal);
+
+            Console.WriteLine("\nDisplaying the names in Animal by ForEach loop");
+            foreach (string a1 in Animal)
+            {
+                Console.WriteLine("\n",a1);
+            }
+            Console.WriteLine("\n-------------------------------------");
+            string[] Places = { "Africa", "New Zealand", "Jamaica", "India" };
+            Array.Sort(Places);
+            Console.WriteLine("\nDisplaying the names in Places by ForEach loop");
+            foreach (string p in Places)
+            {
+            
+                Console.WriteLine("\n",p);
+            }
+        }
+    }
+    }
+
---
+++ ConsoleInheritance/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInheritance
+{
+    interface IVolume
+    {
+        double Volume { get; }
+    }
+    interface IArea
+    {
+        double SurfaceArea();
+    }
+
+    interface ISolid : IArea, IVolume
+    {
+
+    }
+    abstract class Solid : ISolid
+    {
+        double radius;
+        const double pi = 3.142;
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+        public double Pi
+        {
+            get { return pi; }
+
+        }
+
+        public Solid()
+        {
+
+        }
+        public Solid(double _radius)
+
+        {
+            radius = _radius;
+
+        }
+        
+        public abstract double Volume
+        {
+            get;
+        }
+
+       
+        public abstract double SurfaceArea();
+
+
+
+    }
+    class Cylinder : Solid
+    {
+        double height;
+        public Cylinder()
+        {
+
+        }
+        public Cylinder(double _height, double _radius) : base(_radius)
+        {
+            height = _height;
+
+        }
+
+        public override double Volume
+        {
+            get
+            {
+                double volume = Pi * Math.Pow(Radius,2) * height;
+                return volume;
+            }
+
+        }
+        public override double SurfaceArea()
+        {
+            double surfaceArea = 2 * Pi * Radius * (Radius + height);
+            return surfaceArea;
+        }
+    }
+    class Sphere : Solid
+    {
+        public Sphere()
+        {
+
+        }
+        public Sphere(double _radius) : base(_radius)
+        {
+
+        }
+        public override double SurfaceArea()
+        {
+            double surfaceArea = 4 * Pi * Math.Pow(Radius,2);
+            return surfaceArea;
+        }
+        public override double Volume
+        {
+            get
+            {
+                double volume = 4 * Pi * Math.Pow(Radius,3);
+
+                return volume;
+            }
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cylinder cylinder = new Cylinder(5.6, 3.2);
+            Console.WriteLine("Volume of Cylinder : {0}", cylinder.Volume);
+            Console.WriteLine("Surface area  of Cylinder:{0}", cylinder.SurfaceArea());
+            
+
+            Sphere sphere = new Sphere(3.2);
+            Console.WriteLine(" Volume of Sphere :{0}", sphere.Volume);
+            Console.WriteLine("Surface area of Sphere : {0}", sphere.SurfaceArea());
+           
+
+        }
+    }
+}
